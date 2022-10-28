0b6c7c9565a442e36a7448f70412952c2f91b69f
AlexLeXX <alexlexx444@yandex.ru>
2022-10-28 16:22:05 +0500

    Добавил основу Spring Boot для проекта

---
+++ ac-panel/src/main/java/com/freedom/acpanel/AcPanelApplication.java
@@ -0,0 +1,13 @@
+package com.freedom.acpanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcPanelApplication.class, args);
+	}
+
+}
