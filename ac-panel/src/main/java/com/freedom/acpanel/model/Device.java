d95d62b9f69cc283a35d4251f613f5dc2d62e3cf
AlexLeXX <alexlexx444@yandex.ru>
2022-11-14 14:51:21 +0500

    добавил сущность Device

---
+++ ac-panel/src/main/java/com/freedom/acpanel/model/Device.java
@@ -0,0 +1,43 @@
+package com.freedom.acpanel.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.Value;
+
+@Data
+@Entity(name = "devicesDataTable")
+public class Device {
+    @Id
+    private long id;
+    
+    private String name;
+    private int hostId;
+    private int usCode;
+    private long nodeId;
+    private String nodeName;
+    private String type;
+    private String model;
+    private String ipAddress;
+    private int totalNumberOfLogins;
+    private int numberOfLogins;
+
+    @Override
+    public String toString() {
+        return "Device{" + 
+                "id=" + this.id + 
+                ", name='" + this.name + '\'' +
+                ", hostId='" + this.hostId + '\'' +
+                ", usCode='" + this.usCode + '\'' +
+                ", nodeId=" + this.nodeId + '\'' +
+                ", nodeName='" + this.nodeName +
+                ", type='" + this.type +
+                ", model='" + this.model +
+                ", ipAddress='" + this.ipAddress +
+                ", totalNumberOfLogins='" + this.totalNumberOfLogins +
+                ", numberOfLogins='" + this.numberOfLogins +
+                '}';
+    }
+}
---
+++ ac-panel/src/main/java/com/freedom/acpanel/model/DeviceRepository.java
@@ -0,0 +1,7 @@
+package com.freedom.acpanel.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DeviceRepository extends CrudRepository<Device, Long>{
+    
+}
