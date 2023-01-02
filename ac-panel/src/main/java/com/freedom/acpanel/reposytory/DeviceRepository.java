035c76abc0b65d1e1608b653cba328dfeac136e3
AlexLeXX <alexlexx444@yandex.ru>
2023-01-02 18:17:00 +0500

    удалил старые тестовые версии

--- ac-panel/src/main/java/com/freedom/acpanel/reposytory/DeviceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.freedom.acpanel.reposytory;
-
-import com.freedom.acpanel.model.Device;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DeviceRepository extends CrudRepository <Device, Long>{
-    
-}
--- ac-panel/src/main/java/com/freedom/acpanel/reposytory/NodeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.freedom.acpanel.reposytory;
-
-import com.freedom.acpanel.model.Node;
-import org.springframework.data.repository.CrudRepository;
-
-public interface NodeRepository extends CrudRepository <Node, Long>{
-    
-}
