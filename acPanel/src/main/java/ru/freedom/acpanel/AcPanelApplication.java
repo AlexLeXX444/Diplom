08c00618c25b17234d0b2a72efee3a561ad23568
AlexLeXX <alexlexx444@yandex.ru>
2023-01-02 19:49:23 +0500

    пустой каркас

---
+++ acPanel/src/main/java/ru/freedom/acpanel/AcPanelApplication.java
@@ -0,0 +1,13 @@
+package ru.freedom.acpanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcPanelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcPanelApplication.class, args);
+    }
+
+}
---
+++ acPanel/src/test/java/ru/freedom/acpanel/AcPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.freedom.acpanel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcPanelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
